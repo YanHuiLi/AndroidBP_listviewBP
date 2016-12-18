dbfaa8f31233ee71fb65c3bd1b95a0844eff911e
Archer <421228286@qq.com>
2016-12-18 15:28:27 +0800

    隐藏头布局

--- app/src/main/java/com/example/archer/listviewbp/ui/ReFreshListView.java
+++ app/src/main/java/com/example/archer/listviewbp/ui/ReFreshListView.java
@@ -2,6 +2,7 @@ package com.example.archer.listviewbp.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
@@ -51,7 +52,20 @@ public class ReFreshListView extends ListView {
 
         mHeadView = View.inflate(getContext(), R.layout.layout_header_list, null);
 
+        //在设置数据之前执行添加头布局/叫布局方法
         addHeaderView(mHeadView);
+
+        //提前手动测量宽高
+        mHeadView.measure(0,0);//按照设置的规则来测量
+
+        int height = mHeadView.getHeight(); //最后在界面中得到的控件的宽高，必须经过渲染才有值
+        int mHeadViewHeight = mHeadView.getMeasuredHeight();//得到的控件本身大小
+
+
+
+        //设置内边距，可以隐藏当前控件
+        mHeadView.setPadding(0,-mHeadViewHeight,0,0);
+
 
     }
 
