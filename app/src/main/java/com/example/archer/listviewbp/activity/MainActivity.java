02b67325ba7e1526aa3f35629718a6c4c53b5379
Archer <421228286@qq.com>
2016-12-18 11:30:49 +0800

    给自定义的listview添加头布局

---
+++ app/src/main/java/com/example/archer/listviewbp/activity/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.archer.listviewbp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.archer.listviewbp.R;
+import com.example.archer.listviewbp.ui.ReFreshListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ReFreshListView listview;
+    private ArrayList<String> strings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initUI();
+        initData();
+
+
+        initAdapter();
+
+
+    }
+
+    private void initAdapter() {
+
+
+       listview.setAdapter(new MyAdapter());
+
+
+    }
+
+    private void initData() {
+
+        strings = new ArrayList<>();
+        for (int i=0; i<30;i++){
+            strings.add("这是一条数据："+i);
+        }
+
+    }
+
+    private void initUI() {
+
+        listview = (ReFreshListView) findViewById(R.id.list_view);
+    }
+
+    private class MyAdapter  extends BaseAdapter{
+        /**
+         * How many items are in the data set represented by this Adapter.
+         *
+         * @return Count of items.
+         */
+        @Override
+        public int getCount() {
+            return strings.size();
+        }
+
+        /**
+         * Get the data item associated with the specified position in the data set.
+         *
+         * @param position Position of the item whose data we want within the adapter's
+         *                 data set.
+         * @return The data at the specified position.
+         */
+        @Override
+        public Object getItem(int position) {
+            return strings.get(position);
+        }
+
+        /**
+         * Get the row id associated with the specified position in the list.
+         *
+         * @param position The position of the item within the adapter's data set whose row id we want.
+         * @return The id of the item at the specified position.
+         */
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        /**
+         * Get a View that displays the data at the specified position in the data set. You can either
+         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+         * parent View (GridView, ListView...) will apply default layout parameters unless you use
+         * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+         * to specify a root view and to prevent attachment to the root.
+         *
+         * @param position    The position of the item within the adapter's data set of the item whose view
+         *                    we want.
+         * @param convertView The old view to reuse, if possible. Note: You should check that this view
+         *                    is non-null and of an appropriate type before using. If it is not possible to convert
+         *                    this view to display the correct data, this method can create a new view.
+         *                    Heterogeneous lists can specify their number of view types, so that this View is
+         *                    always of the right type (see {@link #getViewTypeCount()} and
+         *                    {@link #getItemViewType(int)}).
+         * @param parent      The parent that this view will eventually be attached to
+         * @return A View corresponding to the data at the specified position.
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            TextView textView=new TextView(MainActivity.this);
+            textView.setText("这是一条数据Freeline"+ strings.get(position));
+
+            textView.setTextSize(18);
+            return textView;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/archer/listviewbp/ui/ReFreshListView.java
@@ -0,0 +1,57 @@
+package com.example.archer.listviewbp.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListView;
+
+import com.example.archer.listviewbp.R;
+
+/**
+ * 自定义一个listview
+ * Created by Archer on 2016/12/15.
+ */
+
+public class ReFreshListView extends ListView {
+
+    private View mHeadView;
+
+    public ReFreshListView(Context context) {
+        super(context);
+        init();
+    }
+
+
+    public ReFreshListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ReFreshListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    private void init() {
+
+        initHeadView();
+        initFootView();
+    }
+
+
+    /**
+     * 添加脚布局
+     */
+    private void initFootView() {
+    }
+
+    /**
+     * 添加头布局
+     */
+    private void initHeadView() {
+
+        mHeadView = View.inflate(getContext(), R.layout.layout_header_list, null);
+
+        addHeaderView(mHeadView);
+
+    }
+
+
+}
